5690b47995b4bbb447b746f1e207c6d640252f09
rockabil <rockabil81@gmail.com>
2025-11-10 00:07:37 +0300

    feat: add implementation of stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,26 @@ const { NotImplementedError } = require('../lib/errors');
  *
  */
 class Stack {
-  push(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.items = [];
+  }
+
+  push(value) {
+    this.items.push(value);
   }
 
   pop() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if(this.items.length === 0) {
+      return undefined;
+    }
+    return this.items.pop();
   }
 
   peek() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (this.items.length === 0) {
+      return undefined;
+    }
+    return this.items[this.items.length - 1];
   }
 }
 
